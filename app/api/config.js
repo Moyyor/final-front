97d00e4f343678a46b6200a93d1aef5250dcec93
Moyyor <149517391+Moyyor@users.noreply.github.com>
2024-05-29 16:56:23 +0300

    добавил проект

---
+++ app/api/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = "http://moyyor.nomorepartiesco.ru/api";
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  getMe: `${BASE_URL}/me`,
+};
--- src/api/config.js
+++
@@ -1,8 +0,0 @@
-export const BASE_URL = "https://moyyor.nomorepartiesco.ru/api";
-
-export const endPoints = {
-	games: `${BASE_URL}/games`,
-	auth: `${BASE_URL}/auth/login`,
-	me: `${BASE_URL}/me`,
-	reg: `${BASE_URL}/auth/local/register`,
-};
