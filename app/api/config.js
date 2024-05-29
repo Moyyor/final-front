babdb4aa290db09d812f1aa76a909f0a4702d377
Moyyor <149517391+Moyyor@users.noreply.github.com>
2024-05-29 21:33:15 +0300

    test

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://moyyor.nomorepartiesco.ru/api";
+export const BASE_URL = "https://moyyor.nomorepartiesco.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
